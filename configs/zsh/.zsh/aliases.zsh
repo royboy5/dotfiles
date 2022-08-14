be14a35dd8bc0ce33cec0289bf8de94bcea1f12f
Roy Eng <reng@flashpoint-intel.com>
2022-08-14 19:39:08 -0400

    added git aliases

--- configs/zsh/.zsh/aliases.zsh
+++ configs/zsh/.zsh/aliases.zsh
@@ -10,3 +10,18 @@ alias ls='exa --color=auto'
 # nvim 
 alias vi="nvim"
 
+# git
+alias ga='git add'
+alias gaa='git add --all'
+alias gb='git branch'
+alias gba='git branch -a'
+alias gbd='git branch -d'
+alias gbD='git branch -D'
+alias gbnm='git branch --no-merged'
+alias gbl='git blame -b -w'
+alias gc='git commit -v'
+alias gcam='git commit -a -m'
+alias gcmsg='git commit -m'
+alias gco='git checkout'
+alias gcb='git checkout -b'
+alias gcf='git config --list'
